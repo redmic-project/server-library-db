1d20639d12e7e6d5100863c3526bb08fdba54ed7
Noel <noelalonso1986@gmail.com>
2021-01-12 14:06:39 +0000

    Elimina refresh al guardar un modelo

--- src/main/java/es/redmic/db/common/service/ServiceRWImpl.java
+++ src/main/java/es/redmic/db/common/service/ServiceRWImpl.java
@@ -88,7 +88,6 @@ public abstract class ServiceRWImpl<TModel extends LongModel, TDTO extends DTO>
 	public TModel persist(TModel model) {
 		try {
 			model = repository.saveAndFlush(model);
-			repository.refresh(model);
 		} catch (DataIntegrityViolationException e) {
 			throw new DBConstraintViolationException(e);
 		} catch (ConstraintViolationException e) {
