344a10566edc9ecef94a860ce5ca788b27dfdd23
Noel <noelalonso1986@gmail.com>
2021-05-06 11:38:50 +0100

    Añade componentes para la gestión del dominio

---
+++ src/main/java/es/redmic/db/maintenance/administrative/model/ThemeInspire.java
@@ -0,0 +1,57 @@
+package es.redmic.db.maintenance.administrative.model;
+
+/*-
+ * #%L
+ * DB
+ * %%
+ * Copyright (C) 2019 - 2021 REDMIC Project / Server
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+import es.redmic.db.common.model.DomainModel;
+
+/**
+ * The persistent class for the themeinspire database table.
+ *
+ */
+@Entity
+@Table(name="themeinspire")
+@NamedQuery(name = "ThemeInspire.findAll", query = "SELECT a FROM ThemeInspire a")
+public class ThemeInspire extends DomainModel implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Column(length = 50)
+	private String code;
+
+	public ThemeInspire() {
+		super();
+	}
+
+	public String getCode() {
+		return this.code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+}
+
